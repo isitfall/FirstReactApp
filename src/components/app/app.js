9ecde12de798f1a3e8b6340404aee33dc90457d8
isitfall <PDAnd4@yandex.ru>
2020-07-24 23:46:07 +0300

    First time use CSS-in-JS technology

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,18 +16,24 @@ import '../post-list/post-list.css';
 import '../post-list-item/post-list-item.css';
 import '../post-status-filter/post-status-filter.css';
 import '../search-panel/search-panel.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
 
 const App = () => {
 
     const data = [
-        {label: "Going to learn React", important: true, id='qweqwe'},
-        {label: "That is so good", important: false, id='wqetrt'},
-        {label: "I need a break...", important: false, id='sdakjsd'}
+        {label: "Going to learn React", important: true, id : 'qweqwe'},
+        {label: "That is so good", important: false, id : 'wqetrt'},
+        {label: "I need a break...", important: false, id : 'sdakjsd'}
     ];
     
 
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex">
                 <SearchPanel/>
@@ -35,7 +41,7 @@ const App = () => {
             </div> 
             <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostListItem from '../post-list-item';
+import { ListGroup } from 'reactstrap';
 
 const PostList = ({posts}) => {
 
@@ -14,9 +15,9 @@ const PostList = ({posts}) => {
     })); 
 
     return(
-       <ul className="app-list list-froup">
+       <ListGroup className="app-list">
             {elements}
-       </ul> 
+       </ListGroup> 
     )
 }
 
